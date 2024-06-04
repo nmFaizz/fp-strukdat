3e1839a749be6fe5c0a84d1b62e846da13bbdd3b
nmFaizz <ilznorz582@gmail.com>
2024-06-04 11:24:41 +0700

    feat: first commit

---
+++ PengeloolaanRestoran/lib/AntrianPesanan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PengeloolaanRestoran.lib
+{
+    public class Node
+    {
+        public string Value;
+        public Node? Next;
+        public Node(string value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+
+    public class AntrianPesanan
+    {
+        private Node Head;
+
+        public AntrianPesanan()
+        {
+            Head = null;
+        }
+
+
+
+    }
+}
